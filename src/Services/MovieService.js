7073cd43947f99cd735be463d47cccbc63be3050
yoshiafk <shayr210@gmail.com>
2021-02-07 21:27:34 +0700

    remove error function

--- src/Services/MovieService.js
+++ src/Services/MovieService.js
@@ -2,7 +2,7 @@ import { CINE_URL } from "../Config/config";
 
 const urls = [
   // `https://cineflixmovieapp.herokuapp.com/api/movies/all?page=1&limit=20`,
-  `http://cors-anywhere.herokuapp.com/${CINE_URL}movies/all`,
+  `${CINE_URL}movies/all`,
   // `${CINE_URL}movies/all?page=2&limit=20`,
   // `${CINE_URL}movies/all?page=3&limit=20`,
 ];
